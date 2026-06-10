fe687ab598a2dcbeda4cd5c0d4732ed84734ec1d
笠原悠磨 <withberry1717@gmail.com>
2026-06-10 12:28:41 +0900

    UI大幅変更

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,1 @@
-
-document.querySelectorAll("img").forEach((img) => {
-  img.addEventListener("error", () => {
-    img.style.display = "none";
-    const box = document.createElement("div");
-    box.className = "missing-image";
-    box.innerHTML = `<strong>画像が見つかりません</strong><br><span>${img.getAttribute("src")}</span>`;
-    box.style.padding = "40px";
-    box.style.color = "#a89d8b";
-    box.style.lineHeight = "1.8";
-    img.parentElement.appendChild(box);
-  });
-});
+document.querySelectorAll("img").forEach((img)=>{img.addEventListener("error",()=>{img.style.display="none";const box=document.createElement("div");box.style.padding="40px";box.style.color="#aaa39a";box.style.lineHeight="1.8";box.innerHTML=`<strong>画像が見つかりません</strong><br><span>${img.getAttribute("src")}</span>`;img.parentElement.appendChild(box);});});
